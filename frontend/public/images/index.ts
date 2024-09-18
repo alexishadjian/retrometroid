fb3d5c9c8964e7ba53ea5823ac3c9fadaffdf653
Alexis <90972106+alexishadjian@users.noreply.github.com>
2024-09-18 11:22:22 +0200

    feat: create hero slide component, create button component

--- frontend/public/images/index.ts
+++ frontend/public/images/index.ts
@@ -1,7 +1,11 @@
 import Logo from './global/logo.webp';
+import HeroSliderImage1 from './home/hero-slider-image-1.webp';
+import HeroSliderImage2 from './home/hero-slider-image-2.webp';
 
 
 
 export {
     Logo,
+    HeroSliderImage1,
+    HeroSliderImage2
 }
