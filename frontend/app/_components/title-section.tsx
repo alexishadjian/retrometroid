5b758029d722ee5711fdd81f41fe1562b99926f1
Alexis <90972106+alexishadjian@users.noreply.github.com>
2024-09-18 14:16:33 +0200

    feat: create TitleSection component

---
+++ frontend/app/_components/title-section.tsx
@@ -0,0 +1,14 @@
+import HeadingButton from "@/components/heading-button";
+
+type Props = {
+    title: string;
+    subtitle: string;
+};
+
+export default function TitleSection({ title, subtitle }: Props) {
+    return (
+        <div className="rounded-2xl py-14 bg-[--light-gray] m-4">
+            <HeadingButton title={title} titleSize={90} titleColor="#000000" subtitle={subtitle} />
+        </div>
+    );
+}
