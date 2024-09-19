47603a4de27802967c604da3a2ffd74e2bf35947
Alexis <90972106+alexishadjian@users.noreply.github.com>
2024-09-19 08:46:34 +0200

    fix: switch font property position

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -9,15 +9,15 @@ const roboto = Roboto({
     subsets: ["latin"],
     style: ['normal', 'italic'],
     weight: ["400", "500", "700"],
-    variable: "--font-roboto",
     display: 'swap',
+    variable: "--font-roboto",
 });
 const bebasNeue = Bebas_Neue({ 
     subsets: ["latin"],
     style: ['normal'],
     weight: ["400"],
-    variable: "--font-bebasNeue",
     display: 'swap',
+    variable: "--font-bebasNeue",
 });
 
 // CSS IMPORT
