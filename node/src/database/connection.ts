7317788dfb3caa38a78caefb4fa712736798a8f0
matlav <mathis.laversin@gmail.com>
2024-09-19 17:14:22 +0200

    Setup docker to start node & mongo

--- node/src/database/connection.ts
+++ node/src/database/connection.ts
@@ -6,11 +6,10 @@ dotenv.config();
 const connectDB = async () => {
   try {
     console.log('MONGO URL', process.env.MONGO_URL);
-    await mongoose.connect(process.env.MONGO_URL!, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    } as mongoose.ConnectOptions);
+    await mongoose.connect(process.env.MONGO_URL as string);
+    console.log('MongoDB connected');
   } catch (err) {
+    console.error('CONSOLE MONGOOOOOOO', process.env.MONGO_URL);
     console.error((err as Error).message);
     process.exit(1);
   }
