73b4a156f43bfd988756fe7c61254ef8a65f4d8c
Matlavv <mathis.laversin92@gmail.com>
2024-10-01 18:04:40 +0200

    minimal setup for codacy

--- backend/test/jest.config.js
+++ backend/test/jest.config.js
@@ -3,4 +3,6 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.test.ts'],
   setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
+  coverageDirectory: './coverage/',
+  collectCoverage: true,
 };
